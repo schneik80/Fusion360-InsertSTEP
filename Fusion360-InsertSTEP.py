5084f5f49b62155ade8a079be587a8e18100699b
Kevin Schneider <schneik80@gmail.com>
2023-10-24 08:53:14 -0700

    Initial Commit

---
+++ Fusion360-InsertSTEP.py
@@ -0,0 +1,41 @@
+#Author-
+#Description-
+
+import adsk.core, adsk.fusion, adsk.cam, traceback
+import os.path
+
+def run(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+
+        msg = ''
+        # Set styles of file dialog.
+        fileDlg = ui.createFileDialog()
+        fileDlg.isMultiSelectEnabled = False
+        fileDlg.title = 'Fusion Insert STEP'
+        fileDlg.filter = '*.stp, *.step'
+
+        # Show file open dialog
+        dlgResult = fileDlg.showOpen()
+        if dlgResult == adsk.core.DialogResults.DialogOK:
+            msg += '\nFiles to Open:'
+            for filename in fileDlg.filenames:
+                msg += '\n\t{}'.format(filename)
+        else:
+            return  
+        
+        adsk.core.Application.log(f'{msg}')
+
+        filename = '"' + filename + '"'
+        
+        command = f'Fusion.ImportComponent {filename}'
+
+        app.executeTextCommand(command)
+
+
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
